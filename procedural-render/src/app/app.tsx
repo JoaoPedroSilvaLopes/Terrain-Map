0217d804a8c9dc30b18e3e20ce0a3c1e7250e2b9
augusto <augustosaaude@gmail.com>
2023-02-23 19:15:53 -0300

    :wrench: chore: add nx workspace in project

---
+++ procedural-render/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="procedural-render" />
+    </StyledApp>
+  );
+}
+
+export default App;

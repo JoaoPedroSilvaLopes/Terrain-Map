55561b2526c5e23f4380eed71063a4f99a71de4a
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-03-16 16:10:35 -0300

    :truck: chore: theme isolated

--- modules/render/procedural-render/src/lib/modules-render-procedural-render.styles.ts
+++ modules/render/procedural-render/src/lib/modules-render-procedural-render.styles.ts
@@ -1,26 +1,28 @@
 import styled from 'styled-components';
+import { theme } from './theme';
 
 export const Wrapper = styled.div`
   margin: 0
   padding: 0
-  boxsizing: border-box;
-  display: flex;
+  box-sizing: border-box;
 
   font-family: 'Poppins', sans-serif;
+  color: ${theme.colors.textPrimary};
 
+  display: flex;
 `;
 
 export const Menu = styled.div`
   display: flex;
   flex-direction: column;
-
   justify-content: center;
   align-items: center;
 `;
 
 export const Title = styled.h1`
+  font-weight: ${theme.fonts.boldWeight};
+
+  font-style: normal;
   font-size: 1.563rem;
-  font-weight: 700;
   line-height: 2.344rem;
-  color: #333333;
 `;
---
+++ modules/render/procedural-render/src/lib/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    textPrimary: '#333333',
+  },
+
+  fonts: {
+    boldWeight: 700,
+  },
+};
